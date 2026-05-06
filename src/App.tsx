fbd7de5b11cc8ae640554b013e41f82201832cc6
root <root@vmi3167737.contaboserver.net>
2026-05-06 17:08:27 +0200

    feat: change heading to te amo chilimichi in pink

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 style={{ color: 'hotpink' }}>Te amo chilimichi</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
